742ac91d84b3482538954f12e4fe75c14f704502
ntquyen56 <quyenb1910287@student.ctu.edu.vn>
2022-09-07 21:47:09 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -7,10 +9,21 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.json({ messgae: "Welcom to contact book application." });
+    res.json({ message: "Welcom to contact book application." });
 });
 
-const contactsRouter = require("./app/routes/contact.route");
+
 app.use ("/api/contacts", contactsRouter);
+
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+app.use((error, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Intern Server Error",
+    });
+});
+
 
 module.exports = app;
